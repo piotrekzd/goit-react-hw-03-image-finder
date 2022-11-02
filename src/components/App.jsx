4b6c4eb47db711a2c88c37e22b3046013e97e1fe
Piotr Zdebski <piotrek.zdebski@gmail.com>
2022-11-02 19:56:48 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,17 @@
+import React from 'react';
+import { fetchImg } from './API/API.js';
+import { Button } from './Button/Button.jsx';
+import { ImageGallery } from './ImageGallery/ImageGallery.jsx';
+import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem.jsx';
+import { Loader } from './Loader/Loader.jsx';
+import { Modal } from './Modal/Modal.jsx';
+import { Searchbar } from './Searchbar/Searchbar.jsx';
 import style from './App.module.css';
 
+
 export const App = () => {
+
+
   return (
     <div className={style.wrapper}>
       React homework template
